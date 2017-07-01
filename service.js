f32de9af07362f407092747ff6187e1496209f15
FernandoCagale <fernando.b.cagale@gmail.com>
2017-07-01 11:00:01 -0300

    add mesh

--- service.js
+++ service.js
@@ -4,13 +4,40 @@ require('dotenv').load({silent: true});
 
 const seneca = require('seneca')();
 const entity = require('seneca-entity');
+const mesh = require('seneca-mesh');
+const consul = require('seneca-consul-registry');
 const mongoStore = require('seneca-mongo-store');
 const auth = require('./lib/auth');
+
+const PINS = [
+  'role:auth,cmd:*',
+];
 
 const opts = {
   mongo: {
     uri: process.env.URI || 'mongodb://127.0.0.1:27017/seneca-auth',
     options: {}
+  },
+  mesh: {
+    tag: 'base',
+    auto: true,
+    host: process.env.ADDR || '127.0.0.1',
+    listen: [{
+      pin: PINS,
+      host: process.env.ADDR || '127.0.0.1',
+      port: process.env.PORT || 50000 + Math.floor((10000 * Math.random()))
+    }],
+    discover: {
+      registry: {
+        active: true
+      },
+      multicast: {
+        active: false
+      }
+    }
+  },
+  consul: {
+    host: process.env.CONSUL_ADDR || '127.0.0.1'
   }
 };
 
